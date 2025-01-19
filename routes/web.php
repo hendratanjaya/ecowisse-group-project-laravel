627bdf1e47066e5369b43839b44026097f652b47
Hendra Tanuwijaya <114296260+hendratanjaya@users.noreply.github.com>
2025-01-19 17:36:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\TransactionController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route("home.page);
 });
 
 Route::middleware(['middleware' => 'ClearCheckoutSession', 'CheckRole:customer'])->prefix('/ecowise')->group(function(){
